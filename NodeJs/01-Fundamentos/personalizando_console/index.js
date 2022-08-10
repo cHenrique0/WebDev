ba3e440be8a0a5a7e01bb51d50b0ab9374122262
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-10 20:15:12 -0300

    customizing node console

---
+++ NodeJs/01-Fundamentos/personalizando_console/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.green("Text em verde"));
+console.log(chalk.bgRed.black("Texto em preto com background vermelho"));
+console.log(chalk.italic("Texto em italico"));
