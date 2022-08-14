4ceca706dc8bb582c1dc522ebb98756204caa07a
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-14 12:19:02 -0300

    add: find account, check account and email exists

--- NodeJs/03-Pratica_ContaBancaria/operations.js
+++ NodeJs/03-Pratica_ContaBancaria/operations.js
@@ -36,6 +36,29 @@ export function createAccount() {
     .then((answers) => {
       const { account, name, age, email, balance } = answers;
 
+      if (checkAccountExists(name)) {
+        console.log(
+          chalk.red.italic(
+            "\nYou already have an account at THE BANK.\nPlease, try remember your account number."
+          )
+        );
+        return setTimeout(() => {
+          console.clear();
+          systemInit();
+        }, 2000);
+      }
+      if (checkEmailExists(email)) {
+        console.log(
+          chalk.red.italic(
+            "\nThis email already exists.\nPlease choose another.\n"
+          )
+        );
+        return setTimeout(() => {
+          console.clear();
+          createAccount();
+        }, 2000);
+      }
+
       const newAccount = {
         account: Number(account),
         name,
@@ -83,29 +106,26 @@ export function checkBalance() {
     })
     .then((answer) => {
       const { account } = answer;
-      const location = getStoragePath();
-      fs.readFile(location, (err, data) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const accountJSON = JSON.parse(data);
-        const accountMatchIndex = accountJSON.accounts
-          .map((acc) => acc.account)
-          .indexOf(Number(account));
-        if (accountMatchIndex === -1) {
-          console.log(
-            "Sorry.\nThis account doesn't exists\nEnter a valid account number.\n"
-          );
-          return systemInit();
-        }
-        const accountMatch = accountJSON.accounts[accountMatchIndex];
-        console.log(`Welcome back, ${chalk.cyan(accountMatch.name)}`);
+      const accountData = findAccount(account);
+      if (!accountData) {
         console.log(
-          `Your balance is: ${chalk.green("R$ " + accountMatch.balance)}\n`
+          chalk.red.italic(
+            "\nSorry. This account doesn't exists\nEnter a valid account number.\n"
+          )
         );
-        return systemInit();
-      });
+
+        return setTimeout(() => {
+          console.clear();
+          checkBalance();
+        }, 1500);
+      }
+
+      console.log(`\nWelcome back, ${chalk.cyan(accountData.name)}`);
+      console.log(
+        `Your balance is: ${chalk.green("R$ " + accountData.balance)}\n`
+      );
+
+      return systemInit();
     });
 }
 
@@ -119,36 +139,32 @@ export function deposit() {
     })
     .then((answer) => {
       const { account } = answer;
-      const location = getStoragePath();
-      fs.readFile(location, (err, data) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const accountJSON = JSON.parse(data);
-        const accountMatchIndex = accountJSON.accounts
-          .map((acc) => acc.account)
-          .indexOf(Number(account));
-        if (accountMatchIndex === -1) {
-          console.log(
-            "Sorry.\nThis account doesn't exists\nEnter a valid account number.\n"
-          );
+      const accountData = findAccount(account);
+      if (!accountData) {
+        console.log(
+          chalk.red.italic(
+            "\nSorry. This account doesn't exists\nEnter a valid account number.\n"
+          )
+        );
+
+        return setTimeout(() => {
+          console.clear();
+          deposit();
+        }, 1500);
+      }
+
+      inquirer
+        .prompt({
+          name: "amount",
+          message: "Enter the amount you want to deposit:",
+        })
+        .then((answer) => {
+          const { amount } = answer;
+          accountData.balance += Number(amount);
+          storeAccount(accountData);
+          console.log(chalk.green("\nDeposit made successfully\n"));
           return systemInit();
-        }
-        const accountMatch = accountJSON.accounts[accountMatchIndex];
-        inquirer
-          .prompt({
-            name: "amount",
-            message: "Enter the amount you want to deposit:",
-          })
-          .then((answer) => {
-            const { amount } = answer;
-            accountMatch.balance += Number(amount);
-            storeAccount(accountMatch);
-            console.log(chalk.green("Deposit made successfully\n"));
-            return systemInit();
-          });
-      });
+        });
     });
 }
 
@@ -162,45 +178,44 @@ export function withdraw() {
     })
     .then((answer) => {
       const { account } = answer;
-      const location = getStoragePath();
-      fs.readFile(location, (err, data) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const accountJSON = JSON.parse(data);
-        const accountMatchIndex = accountJSON.accounts
-          .map((acc) => acc.account)
-          .indexOf(Number(account));
-        if (accountMatchIndex === -1) {
-          console.log(
-            "Sorry.\nThis account doesn't exists.\nEnter a valid account number.\n"
-          );
+      const accountData = findAccount(account);
+      if (!accountData) {
+        console.log(
+          chalk.red.italic(
+            "\nSorry. This account doesn't exists\nEnter a valid account number.\n"
+          )
+        );
+
+        return setTimeout(() => {
+          console.clear();
+          withdraw();
+        }, 1500);
+      }
+
+      inquirer
+        .prompt({
+          name: "amount",
+          message: "Enter the amount you want to withdraw:",
+        })
+        .then((answer) => {
+          const { amount } = answer;
+          if (Number(amount) > accountData.balance) {
+            console.log(
+              chalk.red.italic(
+                `\nDear ${accountData.name},\nYou don't have available balance for this transaction.\n`,
+                `Please, check your balance and try again.\n`
+              )
+            );
+            return setTimeout(() => {
+              console.clear();
+              systemInit();
+            }, 3000);
+          }
+          accountData.balance -= Number(amount);
+          storeAccount(accountData);
+          console.log(chalk.green("Withdrawal successful\n"));
           return systemInit();
-        }
-        const accountMatch = accountJSON.accounts[accountMatchIndex];
-        inquirer
-          .prompt({
-            name: "amount",
-            message: "Enter the amount you want to withdraw:",
-          })
-          .then((answer) => {
-            const { amount } = answer;
-            if (Number(amount) > accountMatch.balance || Number(amount) === 0) {
-              console.log(
-                chalk.bgRed.black(
-                  `\nDear ${accountMatch.name},\nYou don't have available balance for this transaction.\n`,
-                  `Please, check your balance and try again.\n`
-                )
-              );
-              return systemInit();
-            }
-            accountMatch.balance -= Number(amount);
-            storeAccount(accountMatch);
-            console.log(chalk.green("Withdrawal successful\n"));
-            return systemInit();
-          });
-      });
+        });
     });
 }
 
@@ -261,4 +276,68 @@ function storeAccount(account) {
 
 function getStoragePath() {
   return path.resolve("./storage/accounts.json");
+}
+
+function findAccount(account) {
+  const location = getStoragePath();
+  const accountJSON = JSON.parse(
+    fs.readFileSync(location, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+  );
+
+  const accountIndex = accountJSON.accounts
+    .map((acc) => acc.account)
+    .indexOf(Number(account));
+  if (accountIndex === -1) {
+    return;
+  }
+  const accountData = accountJSON.accounts[accountIndex];
+
+  return accountData;
+}
+
+function checkAccountExists(name) {
+  const location = getStoragePath();
+  const accountJSON = JSON.parse(
+    fs.readFileSync(location, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+  );
+
+  const accountIndex = accountJSON.accounts
+    .map((acc) => acc.name)
+    .indexOf(name);
+  if (accountIndex === -1) {
+    return;
+  }
+  const accountData = accountJSON.accounts[accountIndex];
+  return accountData;
+}
+
+function checkEmailExists(email) {
+  const location = getStoragePath();
+  const accountJSON = JSON.parse(
+    fs.readFileSync(location, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+  );
+
+  const accountIndex = accountJSON.accounts
+    .map((acc) => acc.email)
+    .indexOf(email);
+  if (accountIndex === -1) {
+    return;
+  }
+  const accountData = accountJSON.accounts[accountIndex];
+  return accountData;
 }
