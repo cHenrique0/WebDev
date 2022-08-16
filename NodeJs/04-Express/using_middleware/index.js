63f4e8036d778c7f07f1c59e7e71361670c23434
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-16 10:49:00 -0300

    using middleware in express

---
+++ NodeJs/04-Express/using_middleware/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+const basePath = path.join(__dirname, "templates");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// defining middleware function
+const testMiddleware = (request, response, next) => {
+  console.log("Testing middlewares functions");
+  return next();
+};
+
+// use middleware
+app.use(testMiddleware);
+
+app.get("/", (request, response) => {
+  return response.status(200).sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Application is running at port: ${port}`);
+});
