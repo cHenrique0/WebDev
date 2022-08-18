6bd3a8189e12dcc951527dd80b9ea59acfa9f402
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-18 14:30:43 -0300

    find by uuid with sequelize

--- NodeJs/06-SequelizeORM/index.js
+++ NodeJs/06-SequelizeORM/index.js
@@ -35,8 +35,16 @@ app.post("/users/create", async (request, response) => {
   return response.status(201).redirect("/");
 });
 
+// Select: one user
+app.get("/users/:uuid", async (request, response) => {
+  const { uuid } = request.params;
+  const user = await User.findByPk(uuid, { raw: true });
+  // const user = await User.findOne({ where: { uuid }, raw: true });
+  return response.status(200).render("find-user", { user });
+});
+
 app.get("/", async (request, response) => {
-  // Select users
+  // Select: all users
   // raw: true only returns a list of user objects
   // Test findAll without raw and print it on the console
   const users = await User.findAll({ raw: true });
