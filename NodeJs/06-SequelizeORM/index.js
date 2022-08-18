693cf39a6295793119aef7821d93399a3d05f530
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-08-18 11:06:11 -0300

    inserting data in the database with sequelize

--- NodeJs/06-SequelizeORM/index.js
+++ NodeJs/06-SequelizeORM/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const connection = require("./database/connection");
+const User = require("./models/User");
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,25 @@ app.use(express.static("public"));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
+
+app.get("/users/create", (request, response) => {
+  return response.status(200).render("add-user");
+});
+
+app.post("/users/create", async (request, response) => {
+  const { name, occupation } = request.body;
+  const newsletter = request.body.newsletter === "on" ? true : false;
+
+  const newUser = {
+    name,
+    occupation,
+    newsletter,
+  };
+
+  await User.create({ name, occupation, newsletter });
+
+  return response.status(201).redirect("/");
+});
 
 app.get("/", (request, response) => {
   return response.status(200).render("home");
